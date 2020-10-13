b37cc4a1e162412ad5a387813f82952d196a6ef4
Dele <ddelemissis@gmail.auth>
2020-10-13 21:23:17 +0300

    initial commit

--- backend/config.js
+++ backend/config.js
@@ -8,5 +8,5 @@ export default {
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 4000,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 // import fileUpload from 'express-fileupload';
-import path from 'path';
-import uploadRoute from './routes/uploadRoute';
-import userRoute from './routes/userRoute';
-import productRoute from './routes/productRoute';
-import orderRoute from './routes/orderRoute';
-import config from './config';
+import path from "path";
+import uploadRoute from "./routes/uploadRoute";
+import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
+import orderRoute from "./routes/orderRoute";
+import config from "./config";
 
 const mongodbUrl = config.MONGODB_URL;
 const port = config.PORT;
@@ -22,42 +22,42 @@ mongoose
 
 const app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 app.use(bodyParser.json());
 
-app.use('/api/uploads', uploadRoute);
-app.use('/api/products', productRoute);
-app.use('/api/users', userRoute);
-app.use('/api/orders', orderRoute);
+app.use("/api/uploads", uploadRoute);
+app.use("/api/products", productRoute);
+app.use("/api/users", userRoute);
+app.use("/api/orders", orderRoute);
 
-app.get('/api/config/paypal', (req, res) => {
+app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
 
-app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
 
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
-app.get('*', (req, res) => {
+app.use(express.static(path.join(__dirname, "/../frontend/build")));
+app.get("*", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  const status = err.name && err.name === 'ValidationError' ? 400 : 500;
+  const status = err.name && err.name === "ValidationError" ? 400 : 500;
   res.status(status);
   res.send({ message: err.message });
 });
 
 // app.use(fileUpload());
-app.post('/upload', (req, res) => {
+app.post("/upload", (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
+    return res.status(400).send("No files were uploaded.");
   }
   const { image } = req.files;
   const filename = `${new Date().getTime()}.jpg`;
